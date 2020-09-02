20ba578efe39bbc529e482b2ba6d1658ef51bed5
Benjamin Gallusser <bgallusser@googlemail.com>
2020-09-02 17:09:28 -0400

    Remove broken agglomeration imports

--- lsd/__init__.py
+++ lsd/__init__.py
@@ -2,11 +2,11 @@ from __future__ import absolute_import
 from .agglomerate import LsdAgglomeration
 from .local_shape_descriptor import LsdExtractor
 from .merge_tree import MergeTree
-from .parallel_aff_agglomerate import parallel_aff_agglomerate
-from .parallel_fragments import parallel_watershed
+# from .parallel_aff_agglomerate import parallel_aff_agglomerate
+# from .parallel_fragments import parallel_watershed
 from .parallel_lsd_agglomerate import parallel_lsd_agglomerate
-from .rag import Rag
-from .shared_rag_provider import SharedRagProvider, SubRag
+# from .rag import Rag
+# from .shared_rag_provider import SharedRagProvider, SubRag
 from . import fragments
 from . import gp
-from . import persistence
+# from . import persistence
