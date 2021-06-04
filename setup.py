51c1d3b726c1d5ab21ae21d38ed4044ca9a5ae8e
Benjamin Gallusser <bgallusser@googlemail.com>
2021-06-04 12:14:01 +0200

    Remove install of merge_tree cython extension

--- setup.py
+++ setup.py
@@ -1,26 +1,16 @@
 from distutils.core import setup
-from distutils.extension import Extension
 
 setup(
-        name='lsd',
-        version='0.1',
-        description='Local Shape Descriptors.',
-        url='https://github.com/funkey/lsd',
-        author='Jan Funke',
-        author_email='jfunke@iri.upc.edu',
-        license='MIT',
-        packages=[
+    name='lsd',
+    version='0.1',
+    description='Local Shape Descriptors.',
+    url='https://github.com/funkey/lsd',
+    author='Jan Funke',
+    author_email='jfunke@iri.upc.edu',
+    license='MIT',
+    packages=[
             'lsd',
             'lsd.gp',
             'lsd.persistence',
-        ],
-        ext_modules=[
-            Extension(
-                'lsd.merge_tree',
-                sources=[
-                    'lsd/merge_tree.pyx'
-                ],
-                extra_compile_args=['-O3'],
-                language='c++')
-        ]
+    ],
 )
